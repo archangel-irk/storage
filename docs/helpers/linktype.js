8dedc02c8256b3964f7a6659c19870e2cfff0426
Constantine Melnikov <ka.melnikov@gmail.com>
2014-09-27 22:37:46 +0900

    fix docs

--- docs/helpers/linktype.js
+++ docs/helpers/linktype.js
@@ -1,15 +1,15 @@
 
 var types = {};
 types.Object = 'https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Object';
-types.Boolean = 'https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Boolean'
-types.String = 'https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/String'
-types.Array = 'https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Array'
-types.Number = 'https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Number'
-types.Date = 'https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Date'
-types.Function = 'https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Function'
-types.RegExp = 'https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/RegExp'
-types.Error = 'https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Error'
-types['undefined'] = 'https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/undefined'
+types.Boolean = 'https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Boolean';
+types.String = 'https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/String';
+types.Array = 'https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Array';
+types.Number = 'https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Number';
+types.Date = 'https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Date';
+types.Function = 'https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Function';
+types.RegExp = 'https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/RegExp';
+types.Error = 'https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Error';
+types['undefined'] = 'https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/undefined';
 
 // mongoose
 types.Aggregate = '#aggregate_Aggregate';
