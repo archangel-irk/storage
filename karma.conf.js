44817727db4d9b9b18ee6e8f4f558b4ab6235014
Constantine Melnikov <ka.melnikov@gmail.com>
2014-10-11 02:50:42 +0900

    add single karma conf for grunt (future)

---
+++ karma.conf.js
@@ -0,0 +1,104 @@
+module.exports = function (config) {
+  "use strict";
+
+  config.set({
+    // list of files / patterns to load in the browser
+    files: [
+      './vendor/jquery-2.1.1.js',
+      './vendor/lodash.js',
+      './storage.js',
+      './test/*.js'
+    ],
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: [
+      'mocha',
+      'chai'
+    ],
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: [
+      'PhantomJS',
+      'Chrome',
+      'Firefox',
+      'Safari',
+      'IE'
+    ],
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: [
+      'dots'
+    ],
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: true,
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+    sauceLabs: {
+      testName: 'Storage',
+      build: process.env.TRAVIS_JOB_ID,
+      recordScreenshots: false
+    },
+
+    customLaunchers: {
+      sauce_chrome: {
+        base: "SauceLabs",
+        browserName: "chrome",
+        platform: "Windows 7"
+      },
+      sauce_chrome_linux: {
+        base: "SauceLabs",
+        browserName: "chrome"
+      },
+      sauce_firefox: {
+        base: "SauceLabs",
+        browserName: "firefox",
+        platform: "Windows 7"
+      },
+      sauce_firefox_linux: {
+        base: "SauceLabs",
+        browserName: "firefox"
+      },
+      sauce_safari: {
+        base: "SauceLabs",
+        browserName: "safari",
+        platform: "OS X 10.9"
+      },
+      sauce_ie_8: {
+        base: "SauceLabs",
+        browserName: "internet explorer",
+        platform: "Windows XP",
+        version: "8"
+      },
+      sauce_ie_9: {
+        base: "SauceLabs",
+        browserName: "internet explorer",
+        platform: "Windows 7",
+        version: "9"
+      },
+      sauce_ie_10: {
+        base: "SauceLabs",
+        browserName: "internet explorer",
+        platform: "Windows 7",
+        version: "10"
+      },
+      sauce_ie_11: {
+        base: "SauceLabs",
+        browserName: "internet explorer",
+        platform: "Windows 8.1",
+        version: "11"
+      }
+    }
+  });
+};
