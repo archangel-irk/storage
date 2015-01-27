abd399313a4e5693c2a90ddf9a3c2571c658251b
Constantine Melnikov <ka.melnikov@gmail.com>
2015-01-27 11:36:00 +0800

    use min version for tests

--- karma.conf.js
+++ karma.conf.js
@@ -5,7 +5,7 @@ module.exports = function (config) {
     // list of files / patterns to load in the browser
     files: [
       'node_modules/lodash/dist/lodash.js',
-      'dist/storage.debug.js',
+      'dist/storage.min.js',
       'test/*.js'
     ],
 
