4caa367e235716b79b727319c80798a7b771c396
Constantine Melnikov <ka.melnikov@gmail.com>
2015-04-08 23:10:07 +0800

    added test for future

---
+++ test/model(document).test.js
@@ -0,0 +1,17 @@
+/* jshint strict: false */
+/* global describe, it */
+'use strict';
+
+var Schema = storage.Schema
+  , utils = storage.utils
+  , random = utils.random;
+
+var Collection = storage.Collection;
+
+/**
+ * Test.
+ */
+
+describe('model (document)', function(){
+
+});
